f477980dd39247d441fb9e01d5f356e6b2330540
SameerDas <sameerdas63048@gmail.com>
2025-05-20 13:41:43 +0530

    added the cors()

--- Server/app.js
+++ Server/app.js
@@ -12,7 +12,7 @@ let dbconfig=require("./config/dbConfig");
 
 // Allow frontend (http://localhost:5173) to access backend
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // use auth controller routers
